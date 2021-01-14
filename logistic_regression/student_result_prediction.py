0a582512f900ec8131cd8710f0b0f5a6532b3991
kumudlakara <lakara.kumud@gmail.com>
2021-01-14 22:40:20 +0530

    code for logistic regression (student admission prediction)

--- logistic_regression/student_result_prediction.py
+++ logistic_regression/student_result_prediction.py
@@ -3,18 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-#variables to store mean and standard deviation for each feature
-mu = []
-std = []
-
-
 def load_data(filename):
 	df = pd.read_csv(filename, sep=",", index_col=False)
-	df.columns = ["housesize", "rooms", "price"]
+	df.columns = ["test-1", "test-2", "result"]
 	data = np.array(df, dtype=float)
-	#print(data)
-	#plot_data(data[:,:2], data[:, -1])
-	normalize(data)
+	plot_data(data[:,:2], data[:, -1])
 	return data[:,:2], data[:, -1]
 
 def plot_data(x, y):
@@ -26,12 +19,6 @@ def plot_data(x, y):
 		else:
 			plt.plot(x[i,0], x[i,1], 'mD')
 	plt.show()
-
-def normalize(data):
-	for i in range(0,data.shape[1]-1):
-		data[:,i] = ((data[:,i] - np.mean(data[:,i]))/np.std(data[:, i]))
-		mu.append(np.mean(data[:,i]))
-		std.append(np.std(data[:, i]))
 
 def sigmoid(z):
 	return 1/(1 + np.exp(-z))
@@ -61,12 +48,15 @@ def plot_cost(J_all, num_epochs):
 	plt.plot(num_epochs, J_all, 'm', linewidth = "5")
 	plt.show()
 
-def test(theta, x):
-	x[0] = (x[0] - mu[0])/std[0]
-	x[1] = (x[1] - mu[1])/std[1]
+def predict(prob):
+	if(prob >= 0.5):
+		return 1
+	else:
+		return 0
 
+def test(theta, x):
 	y = float(sigmoid(x@theta))
-	if y >= 0.5 :
+	if predict(y) == 1 :
 		print("Admit")
 	else:
 		print("Reject")
@@ -77,8 +67,8 @@ x, y = load_data("student_result_data.txt")
 y = np.reshape(y, (y.shape[0], 1))
 x = np.hstack((np.ones((x.shape[0], 1)), x))
 theta = np.zeros((x.shape[1], 1))
-learning_rate = 0.01
-num_epochs = 5000
+learning_rate = 0.001
+num_epochs = 100
 theta, J_all = gradient_descent(x, y, theta, learning_rate, num_epochs)
 J = cost_function(x, y, theta)
 print(theta)
